024182b8dc719c8690cb6c655593f04d987e202b
amiriz <amir.izhaky@gmail.com>
2020-04-13 16:49:10 +0300

    all basic movement done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,50 +6,43 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    public float jumpHeight;
+    public float jumpSpeed;
+    public LayerMask groundLayers;
     private Rigidbody2D rb2d;
     private SpriteRenderer sr;
+    private BoxCollider2D legsCollider;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
-    }
-
-    void FixedUpdate()
-    {
-        moving();
-        looking();
-        jumping();
-    }
-
-    void moving()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        Vector2 movement = new Vector2(moveHorizontal, 0);
-        rb2d.AddForce(movement * speed);
-    }
-
-    void looking()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-            sr.flipX = true;
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-            sr.flipX = false;
-    }
-
-    void jumping()
-    {
-        Vector2 jumpVec = new Vector2(0, 10);
-        if (Input.GetKeyDown(KeyCode.Z))
-            rb2d.AddForce(jumpVec * jumpHeight);
+        legsCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        LookController();
+        MovementController();
+    }
 
+    private void LookController()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
+            sr.flipX = true;
+        if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
+            sr.flipX = false;
+    }
+
+    private void MovementController()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal") * speed;
+        Vector2 movement = new Vector2(horizontalMovement, rb2d.velocity.y);
+        rb2d.velocity = movement;
+
+        //jump:
+        if (Input.GetKeyDown(KeyCode.Z) && rb2d.velocity.y == 0)
+            rb2d.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
     }
 }
