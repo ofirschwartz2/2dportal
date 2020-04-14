0f2c4bf2408a83a0c41916162f1626983a9c6c81
amiriz <amir.izhaky@gmail.com>
2020-04-14 16:49:14 +0300

    - added climbing functionality. - no climbing sptire due to size bug.

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public float Speed;
+    public float JumpSpeed;
+    public float ClimbSpeed;
+    public LayerMask ClimbObjects;
+    private float _originalGravityScale;
+    private Vector2 _originalSize, _climbingSize;
+    private Rigidbody2D _rb;
+    private SpriteRenderer _sr;
+    private bool _climbing;
+    private Sprite _cavemanSprite, _climbingSprite;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _originalGravityScale = _rb.gravityScale;
+        _originalSize = new Vector2(0.1642804f, 0.1501374f);
+        _climbingSize = new Vector2(0.4508384f, 0.4916594f);
+        _sr = GetComponent<SpriteRenderer>();
+        _climbing = false;
+        _cavemanSprite = Resources.Load<Sprite>("Caveman");
+        _climbingSprite = Resources.Load<Sprite>("Caveman Climbing");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookController();
+        MovementController();
+        JumpController();
+        ClimbDetector();
+    }
+
+    private void LookController()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
+            _sr.flipX = true;
+        if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
+            _sr.flipX = false;
+    }
+
+    private void MovementController()
+    {
+        if (!_climbing)
+        {
+            float horizontalMovement = Input.GetAxis("Horizontal") * Speed;
+            _rb.velocity = new Vector2(horizontalMovement, _rb.velocity.y);
+        }
+
+        if (_climbing)
+        {
+            float verticalMovement = Input.GetAxis("Vertical") * ClimbSpeed;
+            _rb.velocity = new Vector2(_rb.velocity.x, verticalMovement);
+        }
+    }
+
+    private void JumpController()
+    {
+        if (Input.GetKeyDown(KeyCode.Z) && _rb.velocity.y == 0)
+        {
+            _climbing = false;
+            //SpriteChange(_cavemanSprite, _originalSize);
+            _rb.AddForce(Vector2.up * JumpSpeed, ForceMode2D.Impulse);
+        }
+    }
+
+    private void ClimbDetector()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.up, 2, ClimbObjects);
+
+        if (hitInfo.collider != null)
+        {
+            if (!_climbing)
+            {
+                if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+                {
+                    //SpriteChange(_climbingSprite, _climbingSize);
+                    _climbing = true;
+                    _rb.velocity = Vector2.zero;
+                    _rb.gravityScale = 0;
+                }
+                else
+                    _rb.gravityScale = _originalGravityScale;
+            }
+        }
+        else
+        {
+            _climbing = false;
+            _rb.gravityScale = _originalGravityScale;
+        }
+    }
+
+    private void SpriteChange(Sprite newSprite, Vector2 newSize)
+    {
+        _sr.sprite = newSprite;
+       _sr.transform.localScale = new Vector2(newSize.x, newSize.y);
+    }
+}
