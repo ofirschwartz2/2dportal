d6e91d1e753034ae2e08aa7ea9aaa37495e00559
amiriz <amir.izhaky@gmail.com>
2020-04-27 20:51:35 +0300

    add exit on escape key

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,13 +6,16 @@ public class CameraController : MonoBehaviour {
     public GameObject player;
     private Vector3 offset;
 
-    // Start is called before the first frame update
     void Start() {
         offset = transform.position - player.transform.position;
     }
 
-    // Update is called once per frame
     void LateUpdate() {
         transform.position = player.transform.position + offset;
+
+        if (Input.GetKeyDown(KeyCode.Escape) == true)
+        {
+            Application.Quit();
+        }
     }
 }
